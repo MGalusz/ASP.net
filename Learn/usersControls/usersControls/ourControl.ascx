4aa96187bf98ca6926c4834252bfccb31297b4ca
MGalusz <galuszka.mm@gmail.com>
2017-08-23 16:24:20 +0200

    my firn control

---
+++ Learn/usersControls/usersControls/ourControl.ascx
@@ -0,0 +1,18 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ourControl.ascx.cs" Inherits="usersControls.ourControl" %>
+<div>
+    <p>
+        G-Way.pl pomysł na weekend.
+    </p>
+    <p>
+        Bierząca data i czas
+    </p>
+    <p>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </p>
+</div>
+<p>
+    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+</p>
+<asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged1" Width="128px"></asp:TextBox>
+<br />
+<asp:Button ID="Button1" runat="server" Text="Button" />
---
+++ Learn/usersControls/usersControls/ourControl.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace usersControls
+{
+    public partial class ourControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = DateTime.Now.ToLongDateString() + DateTime.Now.ToLongTimeString();
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
